093d633f414d83a95be22014a8dfcd5078379c15
ajaysingh3200 <66829002+ajaysingh3200@users.noreply.github.com>
2024-11-02 23:51:16 +0000

    Update subnet.tf
    correction updated

--- subnet.tf
+++ subnet.tf
@@ -1,11 +1,11 @@
 resource "aws_subnet" "public-subnet-1" {
-    vpc_id             = aws_vpc.main.vpc_id
+    vpc_id             = aws_vpc.main.id
     cidr_block         = "10.0.1.0/24"
     availabilty_zone   = "us-east-1a"
     map_public_ip_on_launch = true
 
     tags = {
-        Name = "public-subnet -1" 
+        Name = "public-subnet-1" 
     }
 
 }
