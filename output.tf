6e3af5b5b1f9fc12a394583dbb7aea592aff9be8
ajaysingh3200 <ajaysingh3200>
2025-11-08 21:46:58 +0000

    Add new public subnet and update output.tf

--- output.tf
+++ output.tf
@@ -7,3 +7,27 @@ output "instance_id" {
  value       = aws_instance.public_instance.id
  description = "Instance ID"
 }
+
+output "public_subnet_1_id" {
+  value       = aws_subnet.public-subnet-1.id
+  description = "ID of public subnet 1"
+}
+
+output "public_subnet_2_id" {
+  value       = aws_subnet.public-subnet-2.id
+  description = "ID of public subnet 2"
+}
+
+output "public_subnet_3_id" {
+  value       = aws_subnet.public-subnet-3.id
+  description = "ID of public subnet 3"
+}
+
+output "public_subnet_cidrs" {
+  value = [
+    aws_subnet.public-subnet-1.cidr_block,
+    aws_subnet.public-subnet-2.cidr_block,
+    aws_subnet.public-subnet-3.cidr_block
+  ]
+  description = "CIDR blocks of all public subnets"
+}
--- subnet.tf
+++ subnet.tf
@@ -21,3 +21,17 @@ resource "aws_subnet" "public-subnet-2" {
    }
 
 }
+
+resource "aws_subnet" "public-subnet-3" {
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = "10.0.3.0/24"
+    availability_zone       = "us-east-1c"
+    map_public_ip_on_launch = true
+
+    tags = {
+    Name = "public-subnet-2"
+   }
+
+}
+
+
